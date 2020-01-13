fe4f13ab5c64806c4d80bc4d52ab8d3f0e28a5b6
Landry Irakoze <irakozel@gmail.com>
2020-01-13 18:02:33 -0500

    exercises started

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -35,6 +35,10 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 # Write a function `print_tuple` that prints all the values in a tuple
 
 # YOUR CODE HERE
+def print_tuple(tuple):
+  for item in tuple:
+    print(item, end=' ')
+  print('\n')
 
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
